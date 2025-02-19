9a2ba17740cbb9b6361da18e52559d17094507bf
Krists Krievins <krists.krievins@gmail.com>
2025-02-19 13:58:13 +0100

    Last Fixes

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
-const part1Done = part1.slice(0, 3) + part1[3].toUpperCase();
-const part2Done = part2.slice(0, 5) + part2[5].toUpperCase();
+const part1Done = part1.slice(0, -1) + part1[part1.length - 1].toUpperCase();
+const part2Done = part2.slice(0, -1) + part2[part2.length - 1].toUpperCase();
 
 const result = part1Done + part2Done;
 
@@ -38,11 +38,11 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
-const tax = 84 * 0.15;
+const tip = 84 * 0.15;
 
 // Print out the tipAmount
 
-console.log(tax);
+console.log(tip);
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
